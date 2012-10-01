6b0c729bb861e312ab060ce130de145141435a09
Romano Kleinwächter <romano@rufus.(none)>
2012-10-01 21:15:04 +0200

    Mt.Gox Streampuffering

--- mtgox.c
+++ mtgox.c
@@ -27,11 +27,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include "mtgox.h"
 #include "btrade.h"
 #include "websocket.h"
 /** ********** /INCLUDES ********* */
 
+
+/** ********** KONFIGURATION ********** */
+static char HOST[] = "websocket.mtgox.com";
+static int PORT = 80;
+static char MONITOR_RES[] = "mtgox";
+/** ********** /KONFIGURATION ********* */
 
 /** ********** FUNKTIONEN ********** */
 /**
@@ -42,7 +49,45 @@
  */
 int mtgox_main(char *currency)
 {
-	printf("Derzeit nicht implementiert.\n");
+	printf("\n!! IN ARBEIT !!\n\n");
+
+	// Ressource mit Währung ergänzen
+	char res[strlen(MONITOR_RES)+strlen(currency)+10+1]; // 10="?Currency="
+	memset(&res, 0x00, sizeof(res));
+	snprintf(res, sizeof(res), "%s?Currency=%s", MONITOR_RES, currency);
+
+	// Verbindung zur Mt.Gox Websocket API aufbauen und
+	// Socketfilediscriptor zurückgeben
+	int sock = websocket_open(HOST, res, PORT);
+
+	// Stream puffern bis ein gültiges JSON enstanden ist
+	// und dieses verarbeiten.
+	char *json=NULL; // Späterer Platz für JSON Daten
+	ssize_t cur_bytes=0, all_bytes=0;
+	while(1)
+	{
+		// Puffer (re)initialisieren
+		char buff[256];
+		memset(&buff, 0x00, 256);
+
+		// Von Stream lesen und Daten merken
+		cur_bytes = read(sock, &buff, sizeof(buff));
+		if(cur_bytes>0)
+		{
+			// Ausstieg
+			if(all_bytes>=1024) break;
+
+			// Speicher dynamsich halten, kopieren & Bytes zählen
+			json = (char*)realloc(json, all_bytes+cur_bytes);
+			memcpy(json+all_bytes, buff, cur_bytes);
+			all_bytes += cur_bytes;
+		}
+	}
+
+	for(size_t i=0; i<all_bytes; ++i) printf("%c", json[i]);
+
+	free(json);
+	websocket_close(sock);
 
 	// alles gut
 	return RET_OK;
